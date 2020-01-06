78f3dea131cf1bebfdba5b5c7cbcd50d3026feff
Eduardo Nascimento <roger.rsn@outlook.com>
2020-01-05 21:04:08 -0300

    init

---
+++ api-series/app/Http/Controllers/TemporadasController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\TemporadaService;
+use App\Http\Controllers\BaseController;
+
+class TemporadasController extends BaseController
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+        $this->classeService = new TemporadaService();
+    }
+
+    public function store(Request $request){
+        
+        return response()
+            ->json(
+                $this->classeService->criarTemporada(
+                    $request->serie_id, $request->numero, $request->eps_por_temporada
+                ),
+                201
+            );
+        
+    }
+
+    public function show(Request $request){
+
+        $id = $request->id;
+      
+        $temporada = $this->classeService->findTemporadaById($id, true);
+
+        if(is_null($temporada)){
+           
+            return response()->json('', 204);
+        }
+        return response()->json($temporada);
+        
+    }
+    
+}
---
+++ api-series/routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+/** @var \Laravel\Lumen\Routing\Router $router */
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+/*Rota de teste
+$router->get('/series', function(){
+    return [
+        "Grey's Anatomy",
+        "Flash"
+    ];
+});
+*/
+//Grupo de rotas
+$router->group(["prefix" => "api"], function() use($router){
+    $router->group(["prefix" => "series"], function() use($router){
+        /* Sem paginacao */
+        //$router->get("", "SeriesController@index");
+
+        /* Com paginacao */
+        $router->get("", "SeriesController@index2");
+        $router->get("{id}", "SeriesController@show");
+        $router->post("", "SeriesController@store");
+        $router->put("{id}", "SeriesController@update");
+        $router->delete("{id}", "SeriesController@destroy");
+    });
+
+    $router->group(["prefix" => "temporadas"], function() use($router){
+        $router->get("", "TemporadasController@index");
+        $router->get("{id}", "TemporadasController@show");
+        $router->post("", "TemporadasController@store");
+        $router->put("{id}", "TemporadasController@update");
+        $router->delete("{id}", "TemporadasController@destroy");
+    });
+
+    $router->group(["prefix" => "episodios"], function() use($router){
+        $router->get("", "EpisodiosController@index");
+        $router->get("{id}", "EpisodiosController@show");
+        $router->post("", "EpisodiosController@store");
+        $router->put("{id}", "EpisodiosController@update");
+        $router->delete("{id}", "EpisodiosController@destroy");
+    });
+    
+});
+
